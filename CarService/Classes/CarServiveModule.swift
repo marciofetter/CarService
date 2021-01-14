c3b822e309831d5e92383240e2a004e067502533
Márcio Fetter <fetter.marcio@gmail.com>
2021-01-14 12:02:10 -0300

    CarServiceModule

---
+++ CarService/Classes/CarServiveModule.swift
@@ -0,0 +1,28 @@
+//
+//  CarServiveModule.swift
+//  CarService
+//
+//  Created by Marcio Fetter on 14/01/21.
+//
+
+import Foundation
+
+public class CarServiceModule {
+    
+    private let client: ServiceCarsProtocol
+    
+    public init(client: ServiceCarsProtocol = ServiceCars()) {
+        self.client = client
+    }
+    
+    public func getCars(completionHandler: (Result<[CarModel], MyError>) -> Void) {
+        client.getCars(completionHandler: completionHandler)
+    }
+    
+    public func getMainViewController(nav: UINavigationController) {
+        let storyboard = UIStoryboard()
+        if let vc = storyboard.instantiateInitialViewController() {
+            nav.pushViewController(vc, animated: true)
+        }
+    }
+}
--- Example/CarService/ViewController.swift
+++ Example/CarService/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        CarServiceModule().getMainViewController(nav: UINavigationController())
         ServiceCars().getCars { (result) in
             
         }
